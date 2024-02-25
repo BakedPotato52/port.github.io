5a89a9f1ef2224a222904c18d99bd66d64d79490
Kanak Acharjee <135026871+BakedPotato52@users.noreply.github.com>
2024-02-25 13:26:55 +0530

    Update Sidebar.js

--- Sidebar.js
+++ Sidebar.js
@@ -11,35 +11,82 @@ const Sidebar = () => {
     };
 
     return (
-        <div className={`sidebar ${isSidebarOpen ? 'sidebar_open' : ''}`}>
+        <div className={`sidebar ${isSidebarOpen ? 'sidebar_open' : ''}` }>
             <button className="sidebar_toggle" onClick={toggleSidebar}>
-                <svg viewBox="0 0 24 24">
-                    <g>
-                        <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-5h18v-2H3v2zm0-5h18v-2H3v2zm0-5h18v-2H3v2zM3 11V9h18v2H3zm0 4h18v-2H3v2zm0-4h18V7H3v2zm0-4h18v-2H3v2zM3 3v2h18V3H3z"></path>
-                    </g>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrows-angle-expand" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707m4.344-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707" />
                 </svg>
             </button>
 
             {isSidebarOpen && (
-                <div className="sidebar">
+                <div>
                     <section className="sidebar_section">
-                        <h2 className="sidebar_section_title">
-                            General
-                        </h2>
-                        <a href="/" className="sidebar_section_link">
-                            <svg viewBox="0 0 24 24">
-                                <g>
-                                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c1.1 0 2-.9 2-2h-4c0 1.1-.9 2-2 2zm-7 0h2v-2H5v2zm0-7h2V7H5v2zM4 8v2h2V8H4zm0-7v2h2V7H4zm7 0v2h2v-2h-2zM4 11V9h18v2H4v2zm0-4h18v-2H4v2zm0-4h18V7H4v2zm0-4h18v-2H4v2zM4 3v2h18V3H4z"></path>
-                            </g>
-                        </svg>
-                        <span>
-                            Dashboard
-                        </span>
-                    </a>
-                    {/* Additional links */}
-                </section>
-                {/* Additional sections */}
-            </div>
+                        <a className="sidebar_section_link sidebar_section_link--selected" href="/">
+                            <svg viewBox="0 0 24 24"><g><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8"></path></g></svg>
+                            <span>
+                                Home
+                            </span>
+                        </a>
+                        <a className="sidebar_section_link sidebar_section_link--selected" href="/">
+                            <svg viewBox="0 0 24 24"><g><path d="M11.23 13.08c-.29-.21-.48-.51-.54-.86-.06-.35.02-.71.23-.99.21-.29.51-.48.86-.54.35-.06.7.02.99.23.29.21.48.51.54.86.06.35-.02.71-.23.99a1.327 1.327 0 01-1.08.56c-.28 0-.55-.08-.77-.25zM22 12c0 5.52-4.48 10-10 10S2 17.52 2 12 6.48 2 12 2s10 4.48 10 10zm-3.97-6.03L9.8 9.8l-3.83 8.23 8.23-3.83 3.83-8.23z"></path></g></svg>
+                            <span>
+                                Explore
+                            </span>
+                        </a>
+                        <a className="sidebar_section_link" href="/">
+                            <svg viewBox="0 0 24 24"><g><path d="M18.7 8.7H5.3V7h13.4v1.7zm-1.7-5H7v1.6h10V3.7zm3.3 8.3v6.7c0 1-.7 1.6-1.6 1.6H5.3c-1 0-1.6-.7-1.6-1.6V12c0-1 .7-1.7 1.6-1.7h13.4c1 0 1.6.8 1.6 1.7zm-5 3.3l-5-2.7V18l5-2.7z"></path></g></svg>
+                            <span>
+                                Subscriptions
+                            </span>
+                        </a>
+                        <a className="sidebar_section_link" href="/">
+                            <svg viewBox="0 0 24 24"><g><path fill-rule="nonzero" d="M21.78 8s-.2-1.37-.8-1.97c-.75-.8-1.6-.8-2-.85C16.2 4.98 12 5 12 5s-4.18-.02-6.97.18c-.4.05-1.24.05-2 .85-.6.6-.8 1.97-.8 1.97s-.2 1.63-.23 3.23v1.7c.03 1.6.23 3.2.23 3.2s.2 1.4.8 2c.76.8 1.75.76 2.2.85 1.57.15 6.6.18 6.77.18 0 0 4.2 0 7-.2.38-.04 1.23-.04 2-.84.6-.6.8-1.98.8-1.98s.2-1.6.2-3.22v-1.7c-.02-1.6-.22-3.22-.22-3.22zm-11.8 7V9.16l5.35 3.03L9.97 15z"></path></g></svg>
+                            <span>
+                                Originals
+                            </span>
+                        </a>
+                    </section>
+
+                    <section className="sidebar_section">
+                        <a className="sidebar_section_link" href="/">
+                            <svg viewBox="0 0 24 24"><g><path fill="none" d="M0 0h24v24H0z"></path><path d="M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-8 12.5v-9l6 4.5-6 4.5z"></path></g></svg>
+                            <span>Library</span>
+                        </a>
+
+                        <a className="sidebar_section_link" href="/">
+                            <svg viewBox="0 0 24 24"><g><path d="M11.9 3.75c-4.55 0-8.23 3.7-8.23 8.25H.92l3.57 3.57.04.13 3.7-3.7H5.5c0-3.54 2.87-6.42 6.42-6.42 3.54 0 6.4 2.88 6.4 6.42s-2.86 6.42-6.4 6.42c-1.78 0-3.38-.73-4.54-1.9l-1.3 1.3c1.5 1.5 3.55 2.43 5.83 2.43 4.58 0 8.28-3.7 8.28-8.25 0-4.56-3.7-8.25-8.26-8.25zM11 8.33v4.6l3.92 2.3.66-1.1-3.2-1.9v-3.9H11z"></path></g></svg>
+                            <span>History</span>
+                        </a>
+
+                        <a className="sidebar_section_link" href="/">
+                            <svg viewBox="0 0 24 24"><g><path d="M18.4 5.6v12.8H5.6V5.6h12.8zm0-1.8H5.6a1.8 1.8 0 0 0-1.8 1.8v12.8a1.8 1.8 0 0 0 1.8 1.9h12.8a1.8 1.8 0 0 0 1.9-1.9V5.6a1.8 1.8 0 0 0-1.9-1.8z"></path><path d="M10.2 9v6.5l5-3.2-5-3.2z"></path></g></svg>
+                            <span>Your Videos</span>
+                        </a>
+
+                        <a className="sidebar_section_link" href="/">
+                            <svg viewBox="0 0 24 24"><g><path d="M12 3.67c-4.58 0-8.33 3.75-8.33 8.33s3.75 8.33 8.33 8.33 8.33-3.75 8.33-8.33S16.58 3.67 12 3.67zm3.5 11.83l-4.33-2.67v-5h1.25v4.34l3.75 2.25-.67 1.08z"></path></g></svg>
+                            <span>Watch later</span>
+                        </a>
+                        <a className="sidebar_section_link" href="/">
+                            <svg viewBox="0 0 24 24"><g><path d="M3.67 8.67h14V11h-14V8.67zm0-4.67h14v2.33h-14V4zm0 9.33H13v2.34H3.67v-2.34zm11.66 0v7l5.84-3.5-5.84-3.5z"></path></g></svg>
+                            <span>Your Playlists</span>
+                        </a>
+
+                        <a className="sidebar_section_link" href="/">
+                            <svg viewBox="0 0 24 24"><g><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path></g></svg>
+                            <span>Show more</span>
+                        </a>
+                    </section>
+                    <section className='sidebar_section' >
+                        <h3>Subscriptions</h3>
+                        <a class="sidebar_section_link" href="/">
+                            <div class=""><img class="feed_item_info_avatar" alt="" src="logo.jpg" /> </div>
+
+                            <span>Kanak Acharya</span>
+                        </a>
+                    </section>
+
+                </div>
             )}
         </div>
     );
